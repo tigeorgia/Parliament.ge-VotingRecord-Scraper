1cf6ae4fd340eae9644d4daec477ebf57542567e
Sebastian Henschel <sebastian@transparency.ge>
2012-02-23 12:19:30 +0400

    Refactored a little bit.

--- scrape.py
+++ scrape.py
@@ -75,13 +75,13 @@ class VotingRecordsScraper (object):
         return tag.attrs[0][1].split('=')[-1]
 
 
-    def bill_det (self, soup):
-        """Find all det (? nomenclature of parliament.ge) on current page.
+    def scrape_details (self, soup):
+        """Find all bill details on current page.
 
-        A det contains info about uri to the actual bill, name and kan_id.
+        A detail contains info about uri to the actual bill, name and kan_id.
 
         @param soup BeautifulSoup.BeautifulSoup
-        @return list of bill records: relative uri, name, kan_id
+        @return list of bill details: relative uri, name, kan_id or None
         """
         regex = re.compile('kan_det=det')
         tags = self.not_in_hidethis(soup.findAll('a', attrs={'href':regex}))
@@ -93,48 +93,72 @@ class VotingRecordsScraper (object):
                 'name': anchor.contents[0].string.strip(),
                 'kan_id': self.get_kan_id(anchor)
             })
+
+        if len(data) == 0:
+            print 'WARNING: No bills found. Is this the right document?'
+            return None
+
         return data
 
 
-    def bill_number (self, soup):
+    def scrape_numbers (self, soup, num_details):
         """Find all bill numbers on current page.
 
         @param soup BeautifulSoup.BeautifulSoup
+        @param num_details number of bill details
+        @throws ScrapeError if number of bill numbers != number of details
         @return list of bill numbers
         """
         regex = re.compile('^\s?\d\S*') # can't mach [-–], so using \S ??
         tags = soup.findAll('td', attrs={'width':'50', 'align':'center'})
         tags = self.not_in_hidethis(tags)
         data = []
+
         for tag in tags:
             if len(tag.contents) == 0: # no bill number
                 data.append(None)
             else:
                 if re.match(regex, tag.contents[0].string):
                     data.append(tag.contents[0].string.strip())
+
+        num = len(data)
+        if num != num_details:
+            fmt = 'Number mismatch: bill numbers %d != bill details %d'
+            raise ScrapeError(fmt % (num, num_details))
+
         return data
 
 
-    def bill_date (self, soup):
+    def scrape_dates (self, soup, num_details):
         """Find all bill dates on current page.
 
         @param soup BeautifulSoup.BeautifulSoup
+        @param num_details number of bill details
+        @throws ScrapeError if number of dates != number of details
         @return list of bill dates
         """
         regex = re.compile('^\d{4}-\d{2}-\d{2}$')
         tags = soup.findAll('td', attrs={'width':80, 'align':'center'})
         tags = self.not_in_hidethis(tags)
         data = []
+
         for tag in tags:
             if re.match(regex, tag.contents[0].string):
                 data.append(tag.contents[0].string)
+
+        num = len(data)
+        if num != num_details:
+            fmt = 'Number mismatch: bill dates %d != bill details %d'
+            raise ScrapeError(fmt % (num, num_details))
+
         return data
 
 
-    def bill_result (self, soup):
+    def scrape_results (self, soup, num_details):
         """Find all bill voting results on current page.
 
         @param soup BeautifulSoup.BeautifulSoup
+        @param num_details number of bill details
         @return list of bill voting results
         """
         regex = re.compile('kan_det=res')
@@ -162,18 +186,24 @@ class VotingRecordsScraper (object):
             handle.close()
             data.append(votes)
             time.sleep(SLEEP) # give the server some time to breathe
+
+        num = len(data)
+        if num != num_details:
+            for _ in xrange(abs(num_details - num)): # fill with empty values
+                data.append('')
+
         return data
 
 
-    def bill_amendments (self, soup, dets):
+    def scrape_amendments (self, soup, details):
         """Find all bill amendments on current page.
 
         @param soup BeautifulSoup.BeautifulSoup
-        @param dets det list
+        @param details list of dicts of bill details
         @return list of bill amendments
         """
         # prepopulate data to be returned
-        data = [[] for _ in xrange(len(dets))]
+        data = [[] for _ in xrange(len(details))]
 
         # each div contains a list of amendments
         tags = soup.findAll('div', attrs={'class':'hidethis'})
@@ -181,9 +211,9 @@ class VotingRecordsScraper (object):
             # find parent bill's index for this div's kan_id
             bill = div.parent.parent.findPreviousSibling()
             kan_id = self.get_kan_id(bill.find('td').find('a'))
-            for det in dets:
+            for det in details:
                 if det['kan_id'] == kan_id:
-                    idx = dets.index(det)
+                    idx = details.index(det)
                     break
 
             # find amendment numbers
@@ -199,7 +229,7 @@ class VotingRecordsScraper (object):
         return data
 
 
-    def next_page (self, soup, is_root=False):
+    def get_next_page (self, soup, is_root=False):
         """Determine the URL of the next page to scrape.
 
         @param soup BeautifulSoup.BeautifulSoup
@@ -218,68 +248,36 @@ class VotingRecordsScraper (object):
         return NEXT_PAGE + limit.encode('utf-8')
 
 
-    def write_record (self, data):
-        """Write a complete voting record to (JSON) file.
+    def write (self, dets, numbers, dates, results, amendments):
+        """Write voting records to (JSON) files.
 
-        @param data dict containing all voting record data
+        @param dets list of dicts containing bill details
+        @param numbers list containing bill numbers
+        @param dates list containing bill dates
+        @param results list of dicts containing voting results
+        @param amendments list containing amendment bill numbers
         """
-        print 'Voting record for bill %s, kan_id %s' % (
-            data['number'], data['kan_id']
-        )
-
-        out = self.outdir + data['kan_id'] + '.json'
-        handle = open(out, 'w')
-        json.dump(data, handle, indent=JSON_INDENT)
-        handle.close()
-
-
-    def page (self, soup):
-        """Scrape a whole page and write it to file.
-
-        @param soup BeautifulSoup.BeautifulSoup
-        """
-        det = self.bill_det(soup)
-        len_det = len(det)
-        if len_det == 0:
-            print 'WARNING: No bills found. Is this the right document?'
-            return False
-
-        number = self.bill_number(soup)
-        len_number = len(number)
-        if len_number != len_det:
-            fmt = 'Number mismatch: bill number %d != bill det %d'
-            raise ScrapeError(fmt % (len_number, len_det))
-
-        date = self.bill_date(soup)
-        len_date = len(date)
-        if len_date != len_det:
-            fmt = 'Number mismatch: bill date %d != bill det %d'
-            raise ScrapeError(fmt % (len_date, len_det))
-
-        result = self.bill_result(soup)
-        len_result = len(result)
-        if len_result != len_det:
-            for i in xrange(abs(len_det - len_result)): # fill with empty values
-                result.append('')
-
-        amendments = self.bill_amendments(soup, det)
-
-        for i in xrange(len_det):
+        for i in xrange(len(dets)):
             record = {
-                'kan_id': det[i]['kan_id'],
-                'url': HOST + det[i]['uri'],
-                'name': det[i]['name'],
-                'number': number[i],
-                'date': date[i],
-                'result': result[i],
+                'kan_id': dets[i]['kan_id'],
+                'url': HOST + dets[i]['uri'],
+                'name': dets[i]['name'],
+                'number': numbers[i],
+                'date': dates[i],
+                'result': results[i],
                 'amendments': amendments[i]
             }
-            self.write_record(record)
 
-        return True
+            fmt = 'Voting record for bill %s, kan_id %s'
+            print fmt % (record['number'], record['kan_id'])
+
+            fname = self.outdir + record['kan_id'] + '.json'
+            handle = open(fname, 'w')
+            json.dump(record, handle, indent=JSON_INDENT)
+            handle.close()
 
 
-    def scrape (self, url, is_root=False):
+    def scrape_page (self, url, is_root=False):
         """Scrape one page and return the next page's URL.
 
         @param url URL of the page to scrape
@@ -287,31 +285,39 @@ class VotingRecordsScraper (object):
         @return string containing URL of the next page to scrape
         """
         time_start = time.time()
-        print 'Scraping %s' % url
+        print 'Scraping: %s' % url
 
         handle = urllib2.urlopen(url)
         soup = BeautifulSoup(handle)
-        self.page(soup)
+
+        details = self.scrape_details(soup)
+        if details:
+            num_details = len(details)
+            numbers = self.scrape_numbers(soup, num_details)
+            dates = self.scrape_dates(soup, num_details)
+            results = self.scrape_results(soup, num_details)
+            amendments = self.scrape_amendments(soup, details)
+            self.write(details, numbers, dates, results, amendments)
+
         handle.close()
 
-        nxt = self.next_page(soup, is_root)
-        print 'Took %d seconds.' % (time.time() - time_start)
+        nxt = self.get_next_page(soup, is_root)
+        print 'Time: Page %d seconds.' % (time.time() - time_start)
+
         return nxt
 
 
     def run (self):
         """Run the scraping process."""
-        print 'Writing to directory %s' % self.outdir
-        time_start_global = time.time()
+        print 'Output directory: %s' % self.outdir
+        time_start = time.time()
 
-        nxt = self.scrape(ROOT, is_root=True)
+        nxt = self.scrape_page(ROOT, is_root=True)
         while nxt:
-            nxt = self.scrape(nxt, is_root=False)
+            nxt = self.scrape_page(nxt, is_root=False)
             time.sleep(SLEEP) # give the server some time to breathe
 
-        print 'The whole scraping took %d seconds.' % (
-            time.time() - time_start_global
-        )
+        print 'Time: Overall %d seconds.' % (time.time() - time_start)
 
 
 def main():
